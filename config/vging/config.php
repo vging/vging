5e99a870c7cdc0bfd7dc60b0170fbd9a27eab335
vging <58787566+vging@users.noreply.github.com>
2019-12-11 17:12:20 -0600

    Add files via upload

---
+++ config/vging/config.php
@@ -0,0 +1,6 @@
+<?php
+define( 'DESTINATARIO',		'comtop.gt@gmail.com' ); //'consultores.geo@gmail.com';
+define( 'CON_COPIA',		'mevo284@gmail.com' );
+define( 'CON_COPIA_OCULTA',	'' );
+define( 'DIR_PRI', 			$_SERVER[ 'DOCUMENT_ROOT' ]."/vging/v1.1" );
+?>
