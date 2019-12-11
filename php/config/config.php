65a8c64d095572fcd68ea4d0874ef87a4d15010f
vging <58787566+vging@users.noreply.github.com>
2019-12-11 17:16:02 -0600

    Update config.php

--- php/config/config.php
+++ php/config/config.php
@@ -1,5 +1,5 @@
 <?php
-define( 'DESTINATARIO',		'comtop.gt@gmail.com' ); //'consultores.geo@gmail.com';
-define( 'CON_COPIA',		'mevo284@gmail.com' );
+define( 'DESTINATARIO', 'vging.gt@gmail.com' ); //'consultores.geo@gmail.com';
+define( 'CON_COPIA', 'mevo284@gmail.com' );
 define( 'CON_COPIA_OCULTA',	'' );
 ?>
